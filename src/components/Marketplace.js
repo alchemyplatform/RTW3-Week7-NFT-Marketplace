2774ee56a42c1ea1c08e7766f14dab69e96376c5
Millie Green <107322691+5r33n@users.noreply.github.com>
2022-12-21 10:49:52 +0000

    update indentation

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -5,51 +5,51 @@ import axios from "axios";
 import { useState } from "react";
 
 export default function Marketplace() {
-const sampleData = [
-    {
-        "name": "NFT#1",
-        "description": "Alchemy's First NFT",
-        "website":"http://axieinfinity.io",
-        "image":"https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
-        "price":"0.03ETH",
-        "currentlySelling":"True",
-        "address":"0xe81Bf5A757CB4f7F82a2F23b1e59bE45c33c5b13",
-    },
-    {
-        "name": "NFT#2",
-        "description": "Alchemy's Second NFT",
-        "website":"http://axieinfinity.io",
-        "image":"https://gateway.pinata.cloud/ipfs/QmdhoL9K8my2vi3fej97foiqGmJ389SMs55oC5EdkrxF2M",
-        "price":"0.03ETH",
-        "currentlySelling":"True",
-        "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
-    },
-    {
-        "name": "NFT#3",
-        "description": "Alchemy's Third NFT",
-        "website":"http://axieinfinity.io",
-        "image":"https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
-        "price":"0.03ETH",
-        "currentlySelling":"True",
-        "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
-    },
-];
-const [data, updateData] = useState(sampleData);
+    const sampleData = [
+        {
+            "name": "NFT#1",
+            "description": "Alchemy's First NFT",
+            "website":"http://axieinfinity.io",
+            "image":"https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
+            "price":"0.03ETH",
+            "currentlySelling":"True",
+            "address":"0xe81Bf5A757CB4f7F82a2F23b1e59bE45c33c5b13",
+        },
+        {
+            "name": "NFT#2",
+            "description": "Alchemy's Second NFT",
+            "website":"http://axieinfinity.io",
+            "image":"https://gateway.pinata.cloud/ipfs/QmdhoL9K8my2vi3fej97foiqGmJ389SMs55oC5EdkrxF2M",
+            "price":"0.03ETH",
+            "currentlySelling":"True",
+            "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
+        },
+        {
+            "name": "NFT#3",
+            "description": "Alchemy's Third NFT",
+            "website":"http://axieinfinity.io",
+            "image":"https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
+            "price":"0.03ETH",
+            "currentlySelling":"True",
+            "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
+        },
+    ];
+    const [data, updateData] = useState(sampleData);
 
-return (
-    <div>
-        <Navbar></Navbar>
-        <div className="flex flex-col place-items-center mt-20">
-            <div className="md:text-xl font-bold text-white">
-                Top NFTs
-            </div>
-            <div className="flex mt-5 justify-between flex-wrap max-w-screen-xl text-center">
-                {data.map((value, index) => {
-                    return <NFTTile data={value} key={index}></NFTTile>;
-                })}
-            </div>
-        </div>            
-    </div>
-);
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div className="flex flex-col place-items-center mt-20">
+                <div className="md:text-xl font-bold text-white">
+                    Top NFTs
+                </div>
+                <div className="flex mt-5 justify-between flex-wrap max-w-screen-xl text-center">
+                    {data.map((value, index) => {
+                        return <NFTTile data={value} key={index}></NFTTile>;
+                    })}
+                </div>
+            </div>            
+        </div>
+    );
 
 }
