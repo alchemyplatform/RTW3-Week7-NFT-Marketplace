86dc66c6addd0fb612880c42d45a34650236b6d2
AkiraMatthew <akira.dev.math@gmail.com>
2024-01-24 20:10:05 -0300

    deployment details

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,25 +3,31 @@ const hre = require("hardhat");
 const fs = require("fs");
 
 async function main() {
-  const [deployer] = await ethers.getSigners();
-  const balance = await deployer.getBalance();
-  const Marketplace = await hre.ethers.getContractFactory("NFTMarketplace");
-  const marketplace = await Marketplace.deploy();
+    const [deployer] = await ethers.getSigners();
+    const balance = await deployer.getBalance();
+    const Marketplace = await hre.ethers.getContractFactory("NFTMarketplace");
+    const marketplace = await Marketplace.deploy();
 
-  await marketplace.deployed();
+    await marketplace.deployed();
 
-  const data = {
-    address: marketplace.address,
-    abi: JSON.parse(marketplace.interface.format('json'))
-  }
+    const data = {
+        address: marketplace.address,
+        abi: JSON.parse(marketplace.interface.format("json")),
+    };
 
-  //This writes the ABI and address to the mktplace.json
-  fs.writeFileSync('./src/Marketplace.json', JSON.stringify(data))
+    console.log(`
+  Contract Address: ${data.address};
+  ABI: ${data.abi}
+  
+  `);
+
+    //This writes the ABI and address to the mktplace.json
+    fs.writeFileSync("./src/Marketplace.json", JSON.stringify(data));
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
